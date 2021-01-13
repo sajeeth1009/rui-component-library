f9dfe89be02bc51c5024472530003712d95b8612
Sajeeth Sadananda <sajeeth.sadanand@gmail.com>
2021-01-13 16:44:10 +0100

    Adding example stylings and auto fixed es lint updates

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react'
 
-import { TestComponentProps } from "./TestComponent.types";
+import { TestComponentProps } from './TestComponent.types'
 
-import "./TestComponent.scss";
+import './TestComponent.scss'
 
 const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
   <div
     data-testid="test-component"
     className={`test-component test-component-${theme}`}
   >
-    <h1 className="heading">I'm the test component</h1>
+    <h1 className="heading">I&aposm the test component</h1>
     <h2>Made with love</h2>
   </div>
-);
+)
 
-export default TestComponent;
+export default TestComponent
